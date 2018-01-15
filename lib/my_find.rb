a19c7fac8aa7353bd8f5b95d7f743ba70e93b0e6
Brent  Ransom <sadumedusa@gmail.com>
2018-01-15 17:59:06 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,8 @@ require 'pry'
 def my_find(collection)
   x = 0
   while x < collection.length
-    yield(collection[x])
+     if yield(collection[x])
+       return collection[x]
     x = x + 1
   end
 end
